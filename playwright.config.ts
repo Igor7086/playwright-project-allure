ec4360b5c9b9cd96dd9fc71fbdc724b7630b07ae
PCW11\Ihor <inaidenko11@gmail.com>
2026-05-11 12:08:50 +0300

    Update gitignore for Allure

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,10 @@ export default defineConfig({
 	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: 'allure-playwright',
+	reporter: [
+  ['list'],
+  ['allure-playwright']
+],
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		baseURL: 'https://qacart-todo.herokuapp.com',
