88cbd0a83bb9afb19e56656862cd85befe3d7294
PCW11\Ihor <inaidenko11@gmail.com>
2026-05-12 13:10:53 +0300

    aiasdei

--- tests/todo.spec.ts
+++ tests/todo.spec.ts
@@ -4,18 +4,53 @@ import SignupPage from '../pages/SignupPage';
 import TodoPage from '../pages/TodoPage';
 import NewTodoPage from '../pages/NewTodoPage';
 
-test('should be able to add and delete a new todo', async ({ page, request, context }) => {
-	const user = new User();
-	const signupPage = new SignupPage();
-	await signupPage.signupUsingAPI(request, user, context);
-	const newTodoPage = new NewTodoPage();
-	await newTodoPage.load(page);
-	await newTodoPage.addTodo(page, 'Learn Playwright!');
-	const todoPage = new TodoPage();
-	const todoItem = await todoPage.getTodoItem(page);
-	expect(await todoItem.innerText()).toEqual('Learn Playwright!');
+// test('should be able to add and delete a new todo', async ({ page, request, context }) => {
+// 	const user = new User();
+// 	const signupPage = new SignupPage();
+// 	await signupPage.signupUsingAPI(request, user, context);
+// 	const newTodoPage = new NewTodoPage();
+// 	await newTodoPage.load(page);
+// 	await newTodoPage.addTodo(page, 'Learn Playwright!');
+// 	const todoPage = new TodoPage();
+// 	const todoItem = await todoPage.getTodoItem(page);
+// 	expect(await todoItem.innerText()).toEqual('Learn Playwright!');
 
-	await todoPage.deleteTodo(page);
-	const noTodosMessage = await todoPage.getNoTodosMessage(page);
-	await expect(noTodosMessage).toBeVisible();
+// 	await todoPage.deleteTodo(page);
+// 	const noTodosMessage = await todoPage.getNoTodosMessage(page);
+// 	await expect(noTodosMessage).toBeVisible();
+// });
+
+test.describe('Todo tests', () => {
+	let newTodoPage: NewTodoPage;
+	let todoPage: TodoPage;
+
+	test.beforeEach(async ({ page, request, context }) => {
+		const user = new User();
+
+		const signupPage = new SignupPage();
+		await signupPage.signupUsingAPI(request, user, context);
+
+		newTodoPage = new NewTodoPage();
+		await newTodoPage.load(page);
+
+		todoPage = new TodoPage();
+	});
+
+	test('should be able to add a new todo', async ({ page }) => {
+		await newTodoPage.addTodo(page, 'Learn Playwright!');
+
+		const todoItem = await todoPage.getTodoItem(page);
+
+		await expect(todoItem).toHaveText('Learn Playwright!');
+	});
+
+	test('should be able to delete a new todo', async ({ page }) => {
+		await newTodoPage.addTodo(page, 'Hello TypeScript!');
+
+		await todoPage.deleteTodo(page);
+
+		const noTodosMessage = await todoPage.getNoTodosMessage(page);
+
+		await expect(noTodosMessage).toBeVisible();
+	});
 });
